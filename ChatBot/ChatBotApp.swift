6f36f6c627af30d1586ebe3f48c3bed3b1a37dd2
Adam Young <me@adam-young.co.uk>
2021-01-25 19:41:58 +0000

    Initial Commit

---
+++ ChatBot/ChatBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatBotApp.swift
+//  ChatBot
+//
+//  Created by Adam Young on 25/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct ChatBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
